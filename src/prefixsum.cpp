c267be09ad24b205d7ef8c55154c8e43dd2514d2
Maxim saprykin <saprykinoff@inbox.ru>
2025-10-28 19:59:23 +0300

    style

--- src/prefixsum.cpp
+++ src/prefixsum.cpp
@@ -3,29 +3,22 @@
 #include "kernels/defines.h"
 #include "wrappers.h"
 
-void calcPrefixSum(
-    gpuptr::u32 a,
-    gpuptr::u32 b,
-    gpuptr::u32 c,
-    unsigned int n,
-    unsigned int depth)
-{
+void calcPrefixSum(gpuptr::u32 a, gpuptr::u32 b, gpuptr::u32 c, unsigned int n,
+                   unsigned int depth) {
     int bsz = (n + GROUP_SIZE - 1) / GROUP_SIZE;
-    if (n <= GROUP_SIZE) { // trivial
+    if (n <= GROUP_SIZE) {  // trivial
         cuda::prefixsum_main(gpu::WorkSize(GROUP_SIZE, n), a, c, n);
         return;
     }
     cuda::prefixsum_pre(gpu::WorkSize(GROUP_SIZE, n), a, b, c, n);
     // need to calculate pref for b[bbase;bbase+bsz)
-    calcPrefixSum(
-        b, // start is b[bbase]
-        b + bsz, // buffer starts at b[bbase+bsz]
-        b, // calulate prefsum inplace
-        bsz, depth + 1);
+    calcPrefixSum(b,        // start is b[bbase]
+                  b + bsz,  // buffer starts at b[bbase+bsz]
+                  b,        // calulate prefsum inplace
+                  bsz, depth + 1);
     cuda::prefixsum_post(gpu::WorkSize(GROUP_SIZE, n), b, c, n);
 }
 
-void prefixSum(gpuptr::u32 in, gpuptr::u32 out, gpuptr::u32 buffer)
-{
+void prefixSum(gpuptr::u32 in, gpuptr::u32 out, gpuptr::u32 buffer) {
     calcPrefixSum(in, buffer, out, in.size(), 0);
 }
--- src/wrappers.h
+++ src/wrappers.h
@@ -4,47 +4,29 @@
 
 namespace gpuptr {
 class u32 {
-public:
-    explicit u32(gpu::gpu_mem_32u& origin)
-        : buffer_(origin)
-        , base_(0)
-    {
-    }
+   public:
+    explicit u32(gpu::gpu_mem_32u& origin) : buffer_(origin), base_(0) {}
     explicit u32(gpu::gpu_mem_32u& origin, unsigned int base)
-        : buffer_(origin)
-        , base_(base)
-    {
-    }
+        : buffer_(origin), base_(base) {}
 
-    u32& operator+=(int x)
-    {
+    u32& operator+=(int x) {
         base_ += x;
         return *this;
     }
 
-    friend u32 operator+(u32 a, int x)
-    {
+    friend u32 operator+(u32 a, int x) {
         a += x;
         return a;
     }
 
-    int size() const
-    {
-        return buffer_.number() - base_;
-    }
-    
-    auto cuptr()
-    {
-        return buffer_.cuptr() + base_;
-    }
+    int size() const { return buffer_.number() - base_; }
 
-    auto cuptr() const
-    {
-        return buffer_.cuptr() + base_;
-    }
+    auto cuptr() { return buffer_.cuptr() + base_; }
 
-private:
+    auto cuptr() const { return buffer_.cuptr() + base_; }
+
+   private:
     gpu::gpu_mem_32u& buffer_;
     unsigned int base_;
 };
-} // namespace gpuptr
+}  // namespace gpuptr
